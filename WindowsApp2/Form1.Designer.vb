13486f712b8551bf6540a8910ce22487cfdc2f3d
Fahad Ahmad <fahadahmad2001@outlook.com>
2018-05-27 10:52:13 +0300

    Add project files.

---
+++ WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(34, 95)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 49)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Extract from FileZilla installation"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column4, Me.Column2, Me.Column3})
+        Me.DataGridView1.Location = New System.Drawing.Point(34, 167)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.Size = New System.Drawing.Size(585, 203)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Host"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Port"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "User Name"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Password"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(152, 95)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 49)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Extract from file"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "select the file"
+        Me.OpenFileDialog1.Filter = "FileZilla password files (recentservers.xml) |recentservers.xml| XML files (*.xml" &
+    ")|*.xml|All Files |*.*"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(258, 109)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "(File path)"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 66)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(177, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(274, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "FileZilla password recovery"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(519, 27)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 25)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Disclaimer"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(631, 387)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "FileZilla password recovery"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ WindowsApp2/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.IO
+Imports System.Xml
+Imports System.Text.RegularExpressions
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "(From FileZilla Installation)"
+        Dim ReadXML As StreamReader
+        Dim PassXML As String
+        Dim XReader
+        Dim xmldoc As New XmlDataDocument()
+        Dim xmlnode As XmlNodeList
+        Dim Str As String
+        'MsgBox(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))
+        DataGridView1.Rows.Clear()
+        ReadXML = New StreamReader(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\FileZilla\recentservers.xml")
+        PassXML = ReadXML.ReadToEnd()
+        ReadXML.Close()
+        'MsgBox(PassXML)
+        XReader = XElement.Parse(PassXML)
+        'MsgBox(PassXML)
+        'Dim xmldoc As New XmlDataDocument()
+        'Dim xmlnode As XmlNodeList
+        If xmldoc.HasChildNodes = True Then
+            xmldoc.DocumentElement.RemoveAll()
+        End If
+        xmldoc.LoadXml(PassXML)
+        xmlnode = xmldoc.GetElementsByTagName("Server")
+        'Dim Str As String
+        Dim output1() As String
+        For count = 0 To xmlnode.Count - 1
+            xmlnode(count).ChildNodes.Item(0)
+            Str = xmlnode(count).ChildNodes.Item(0).InnerText.Trim() & " " & xmlnode(count).ChildNodes.Item(1).InnerText.Trim() & " " & xmlnode(count).ChildNodes.Item(4).InnerText.Trim()
+            Dim TempByte As Byte() = Convert.FromBase64String(xmlnode(count).ChildNodes.Item(5).InnerText.Trim())
+            Dim TempPass As String = System.Text.Encoding.UTF8.GetString(TempByte)
+            Str = Str & " " & TempPass
+            output1 = Str.Split(" ")
+            DataGridView1.Rows.Add(output1(0), output1(1), output1(2), output1(3))
+        Next
+        'Dim output1() As String
+        'output1 = Regex.Split()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim columns As DataGridTextBoxColumn
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim ReadXML As StreamReader
+            Dim PassXML As String
+            Dim XReader
+            Dim xmldoc As New XmlDataDocument()
+            Dim xmlnode As XmlNodeList
+            'Dim Str As String
+            Label1.Text = OpenFileDialog1.FileName
+            DataGridView1.Rows.Clear()
+            ReadXML = New StreamReader(OpenFileDialog1.FileName)
+            PassXML = ReadXML.ReadToEnd()
+            ReadXML.Close()
+            'MsgBox(PassXML)
+            XReader = XElement.Parse(PassXML)
+            'MsgBox(PassXML)
+            'Dim xmldoc As New XmlDataDocument()
+            'Dim xmlnode As XmlNodeList
+            'xmldoc.
+            xmldoc.LoadXml(PassXML)
+            XmlNode = xmldoc.GetElementsByTagName("Server")
+            Dim Str As String
+            Dim output1() As String
+            For count = 0 To XmlNode.Count - 1
+                XmlNode(count).ChildNodes.Item(0)
+                Str = XmlNode(count).ChildNodes.Item(0).InnerText.Trim() & " " & XmlNode(count).ChildNodes.Item(1).InnerText.Trim() & " " & XmlNode(count).ChildNodes.Item(4).InnerText.Trim()
+                Dim TempByte As Byte() = Convert.FromBase64String(XmlNode(count).ChildNodes.Item(5).InnerText.Trim())
+                Dim TempPass As String = System.Text.Encoding.UTF8.GetString(TempByte)
+                Str = Str & " " & TempPass
+                output1 = Str.Split(" ")
+                DataGridView1.Rows.Add(output1(0), output1(1), output1(2), output1(3))
+            Next
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("This program is in no way associated with the FileZilla project or its creators" & vbCrLf & "The author is not responsible for any misuse of this tool", Title:="Disclaimer")
+    End Sub
+End Class
